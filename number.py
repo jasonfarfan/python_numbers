fa0d27b785364ae3514fc24020e172b66ff38aa0
jasonfarfan <jasonfarfan@mail.weber.edu>
2023-02-08 18:49:20 -0700

    random number

--- number.py
+++ number.py
@@ -1,1 +1,11 @@
 # python numbers
+import random 
+#anumber = 10
+#afloatNumber = 2.2
+#print(type(afloatNumber))
+
+#anumber = float(8.8)
+#print(anumber)
+#print( anumber + afloatNumber)
+
+print(random.randrange(0,999))
